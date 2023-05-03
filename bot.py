6805d236c15821c8fa5e1e2ccd1c9c6140b24f52
Myroslava Andrunevchyn <mhnatyuk@gmail.com>
2023-05-03 17:06:06 +0300

    extended filters

--- bot.py
+++ bot.py
@@ -3,6 +3,14 @@ from datetime import datetime, timezone
 from token_data import Token
 import time
 import telebot
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+#logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',
+# datefmt='%d-%b-%y %H:%M:%S')
+
 
 #token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
 # .eyJjcmVhdGVkQXQiOjE2ODIwNjIwOTg2NzksImVtYWlsIjoic21pdHRpYXJrYUBnbWFpbC5jb20iLCJhY3Rpb24iOiJ0b2tlbi1hcGkiLCJpYXQiOjE2ODIwNjIwOTh9.FM1ZZkT9Xa70MKs9L_HTIpalCU6SdSaC1kxVyny6d2Q'
@@ -19,12 +27,12 @@ def command_start(message):
     bot.reply_to(message, "Hello, now I'll be sending you updates on tokens")
     while True:
         send_updates(chat_id)
-        time.sleep(60)
+        time.sleep(2400)
 
 
 def get_account_transactions_url(account):  # returns url for Solscan Account transactions
     beginning = 'https://api.solscan.io/account/token/txs?address='
-    end = '&offset=0&limit=10'
+    end = '&offset=0&limit=20'
     return beginning+account+end
 
 def get_data(url):
@@ -33,9 +41,10 @@ def get_data(url):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
+        logging.debug(f'Raw data status -- {response.status_code}')
         return data
     except requests.exceptions.HTTPError as e:
-        print("Http Error:", e)
+        logging.error("Http Error occured during raw data extraction process", exc_info=True)
 
 
 def get_transactions(data):  # process transaction data and filter by LastChangeID, output list of new transactions
@@ -43,13 +52,15 @@ def get_transactions(data):  # process transaction data and filter by LastChange
     transactions_data = data['data']['tx']['transactions']
     transactions = []
     global last_change_id
+    logging.debug(f'Last change id (since last retrieve) -- {last_change_id}')
     for transaction in transactions_data:
         if transaction['change']['_id'] == last_change_id:
-            print("Transaction break")
+            logging.debug("Transaction break, reached the id of previously retrieved transaction")
             break
         transactions.append(transaction)
     last_change_id = transactions_data[0]['change']['_id']
-    #print(f'Last change id assignment: {last_change_id}')
+    logging.debug(f'Last change id (the id of last transaction in this bulk) -- {last_change_id}')
+    logging.debug(f'List of transactions(no filter) -- {transactions}')
     return transactions
 
 
@@ -57,33 +68,26 @@ def process_transactions(transactions):
     filtered_transactions = {}
     for transaction in transactions:
         if str(transaction['change']['changeAmount']) == '1':
-            #print('Token with +1 value')
-            #print("here it is ", transaction['blockTime'], transaction['change']['changeAmount'])
+            logging.debug(f"Token with +1 value found -- blockTime -- {transaction['blockTime']} -- changeAmount -- {transaction['change']['changeAmount']}")
             if 'tokenName' in transaction['change']:
-                #print('Token has a name')
+                logging.debug(f"Token has a name -- {transaction['change']['tokenName']}")
                 if "Cathlete" in transaction["change"]["tokenName"]:
-                    #print('Transaction with Cathlete')
+                    logging.debug('Transaction with Cathlete')
                     transaction_date = datetime.fromtimestamp(transaction['blockTime'], timezone.utc).isoformat(' ').replace("+00:00",
                     " (UTC)")
                     filtered_transactions[transaction_date] = 'https://api.solscan.io/account?address=' + transaction["change"][
                         "tokenAddress"]
-                    #filtered_transactions[transaction['blockTime']]='https://api.solscan.io/account?address=' + transaction["change"]["tokenAddress"]
-                    #print(f'Transaction added to dict {transaction["slot"]}')
-                    #print(f'Token name: {transaction["change"]["tokenName"]}')
-                    #print(f'Token address: {transaction["change"]["tokenAddress"]}')
-                #else:
-                    #print('Token name not Cathlete')
+                    logging.debug(f"Transaction with slot {transaction['slot']} -- with date {transaction_date} is "
+                                  f"added")
             else:
                 transaction_date = datetime.fromtimestamp(transaction['blockTime'], timezone.utc).isoformat(
                     ' ').replace("+00:00", " (UTC)")
                 filtered_transactions[transaction_date] = 'https://api.solscan.io/account?address=' + \
                                                           transaction["change"]["tokenAddress"]
+                logging.debug(f'Transaction with slot -- {transaction["slot"]} -- with date {transaction_date} -- no '
+                              f'name but link is added')
 
-                #filtered_transactions[transaction['slot']] = 'https://api.solscan.io/account?address=' + transaction[
-                 #   "change"]["tokenAddress"]
-                #print(f'No token name, transaction added {transaction["slot"]}')
-                #print(f'Token address: {transaction["change"]["tokenAddress"]}')
-    #print(f'Filtered transactions: \n{filtered_transactions}')
+    logging.debug(f'Filtered transactions: \n{filtered_transactions}')
     return filtered_transactions
 
 
@@ -91,57 +95,52 @@ def get_external_link(tokens_data: dict):
     external_links = {}
     for key, value in tokens_data.items():
         int_token_data = get_data(value)
-        #print(f'Internal token data:\n{int_token_data}')
+        logging.debug(f'Internal token data:\n{int_token_data}')
         ipfs_link = int_token_data['data']['metadata']['data']['uri']
-        #print(f'IPFS link: \n{ipfs_link}')
+        logging.debug(f'IPFS link: \n{ipfs_link}')
         ipfs_data = get_data(ipfs_link)
-        #print(f'ipfs data: \n{ipfs_data}')
+        logging.debug(f'ipfs data: \n{ipfs_data}')
         external_link = ipfs_data['external_url']
-        #print(f'External link: \n{external_link}')
-        external_links[key]=external_link
-    #print(f'External links: \n{external_links}')
+        logging.debug(f'External link: {external_link}')
+        if ipfs_data['attributes'][1]['value'] == 'Uncommon' or ipfs_data['attributes'][1]['value'] == 'Rare':
+            logging.debug(f'Token filtered by rarity')
+            logging.debug(f'Token added -- {key} -- {external_link} ')
+            external_links[key]=external_link
+    logging.debug(f'External links: \n{external_links}')
     return external_links
 
 
-def tokens(external_links: dict):
-    tokens = []
-    for key, value in external_links.items():
-        external_token_data = get_data(value)
-        print(f'External token data: \n{external_token_data}')
-        token_attr = {'strength': external_token_data['attributes'][2]['value'], 'stamina': external_token_data['attributes'][3][
-            'value'], 'speed': external_token_data['attributes'][4]['value']}
-        print(f'Token attributes: \n{token_attr}')
-        max_value = max(token_attr, key=token_attr.get)
-        token = {'name': external_token_data['attributes'][0]['value'], 'rarity': external_token_data['attributes'][1]['value'],
-                max_value: token_attr[max_value]}
-        print(f'Token: \n{token}')
-        tokens.append(token)
-    print(f'Tokens: \n{tokens}')
-    return tokens
-
-
 def send_updates(chat_id):
-    print(f'Chat id {chat_id}')
+    logging.info(f'Chat id: {chat_id}')
     account_url = get_account_transactions_url(account)
     account_data = get_data(account_url)
-    print('Got account data')
+    logging.debug(f'Account data retrieved')
+
     new_transactions = get_transactions(account_data)
-    print('Got new transactions')
+    logging.debug(f'New transactions retrieved')
+
     filtered_transactions = process_transactions(new_transactions)
-    print('Filtered transactions')
+    logging.debug('Filtered transactions received')
+
     external_links = get_external_link(filtered_transactions)
-    print('Got external links')
+    logging.debug('External links retrieved')
+
     tokens_list = []
     for key, value in external_links.items():
         try:
-            token_obj = Token(value)
-            print('Got token')
+            token_obj = Token(value, key)
+            logging.debug(f'Token\n{token_obj.__str__()}')
+            if token_obj.breed_count != 0:
+                logging.debug(f'Token skipped: {token_obj.breed_count} {token_obj.path}')
+                break
+            logging.debug(f'Token with breed 0 added: {token_obj}')
             tokens_list.append(token_obj)
         except:
-            print(f"Token {key, value} is not available")
-    print('Got tokens list')
+            logging.error(f"Token {key, value} is not available", exc_info=True)
+    logging.debug(f'Tokens list {tokens_list}')
     for token in tokens_list:
         token_str = token.__str__()
         bot.send_message(chat_id=chat_id, text=token_str)
+        logging.debug(f'TG message sent')
 
 bot.polling()
--- token_data.py
+++ token_data.py
@@ -1,33 +1,42 @@
 from playwright.sync_api import sync_playwright
 import html_to_json
+import logging
+from playwright_stealth import stealth_sync
 
+logger = logging.getLogger(__name__)
 
 class Token:
-    def __init__(self, path):
+    def __init__(self, path, time):
         self.path = path
+        self.time = time
         self.full_token_info()
 
     def __repr__(self):
         return f'Name: {self.name}\nRarity: {self.rarity}\nEnergy: {self.energy}\nBreed count: ' \
                f'{self.breed_count}\nLevel: {self.level}\nGems current: {self.gems_current}\nGems lef: ' \
                f'{self.gems_left}\nStrength: {self.strength}\nStamina: {self.stamina}\nSpeed: {self.speed}\nBody: ' \
-               f'{self.body}\nEars: {self.ears}\nFace: {self.face}\n Path: {self.path}'
+               f'{self.body}\nEars: {self.ears}\nFace: {self.face}\n Path: {self.path}\n Time: {self.time}\n Max ' \
+               f'attribute: {self.max_attr}'
 
     def __str__(self):
-        return f'Name: {self.name}\nRarity: {self.rarity}\nEnergy: {self.energy}\nBreed count: ' \
-               f'{self.breed_count}\nLevel: {self.level}\nGems current: {self.gems_current}\nGems lef: ' \
-               f'{self.gems_left}\nStrength: {self.strength}\nStamina: {self.stamina}\nSpeed: {self.speed}\nBody: ' \
-               f'{self.body}\nEars: {self.ears}\nFace: {self.face}'
+        return f"{self.name}+'  '+{self.rarity}+'  '+{self.level}+'/'+{self.breed_count}+'  '+'Max:'+' '+" \
+               f"{self.max_attr}\n'Body: '+{self.body}\nEars: {self.ears}\nFace: {self.face}\n'Time: '+{self.time}"
 
 
     def full_token_info(self):
         with sync_playwright() as p:
             for browser_type in [p.chromium]:
-                browser = browser_type.launch()
-                page = browser.new_page()
+                browser = browser_type.launch(headless=True, timeout=60000)
+                context = browser.new_context(extra_http_headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                    'Accept-Language': 'en-US,en;q=0.9'})
+                page = context.new_page()
+
+                #stealth_sync(page)
                 page.goto(self.path)
                 page.wait_for_selector("span[class^=\"RarityItem__Text\"]")
                 page_content = page.content()
+                logging.debug('Playwright extracted page content')
                 output = html_to_json.convert(page_content)
                 browser.close()
         self.name = self.get_name(output)
@@ -43,6 +52,7 @@ class Token:
         self.body = self.get_body(output)
         self.ears = self.get_ears(output)
         self.face = self.get_face(output)
+        self.max_attr = self.get_max_attr()
 
 
     def get_name(self, output):
@@ -71,9 +81,7 @@ class Token:
     def get_breed_count(self, output):
         try:
             return output['html'][0]['body'][0]['div'][0]['main'][0]['div'][0]['div'][0]['div'][0]['div'][0]['div'][0][
-                'div'][1]['div'][2]['div'][1]['div'][0]['div'][0]['span'][0]['_value'] + '/' + \
-            output['html'][0]['body'][0]['div'][0]['main'][0]['div'][0]['div'][0]['div'][0]['div'][0]['div'][0]['div'][
-                1]['div'][2]['div'][1]['div'][0]['div'][0]['span'][2]['_value']
+                'div'][1]['div'][2]['div'][1]['div'][0]['div'][0]['span'][0]['_value']
         except:
             return "Breed count not available"
 
@@ -151,3 +159,9 @@ class Token:
                 'div'][3]['div'][0]['div'][0]['div'][0]['div'][1]['div'][5]['div'][0]['span'][1]['_value']
         except:
             return "Face not available"
+
+    def get_max_attr(self):
+        stamina = int(self.stamina.split('/')[0])
+        speed = int(self.speed.split('/')[0])
+        strength = int(self.strength('/')[0])
+        return max(strength, stamina, speed)
